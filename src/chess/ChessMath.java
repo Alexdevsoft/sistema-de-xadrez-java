b410e823b652c96ceeb59db275a4b8255acfe841
Alexdevsoft <alexhavilla2022@gmail.com>
2022-12-03 09:57:24 -0300

    movimentando a peça

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -37,6 +37,10 @@ public class ChessMath {
 		if(!board.thereIsApiece(position)){
 			throw new ChessException("Não existe peça na posição de origem.");
 		}
+		
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Não existe movimentos possíveis para a peça escolheida.");
+		}
 	}
 	
 	private Piece makeMove(Position source, Position target) {
