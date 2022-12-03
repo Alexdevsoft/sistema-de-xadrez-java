4e82362c88727c46ea76923dddcf546f3b05b0b6
Alexdevsoft <alexhavilla2022@gmail.com>
2022-12-03 09:27:48 -0300

    Limpando tabuleiro

--- src/xadrez/Program.java
+++ src/xadrez/Program.java
@@ -1,7 +1,9 @@
 package xadrez;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMath;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -15,24 +17,34 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		ChessMath chessMath = new ChessMath();
-		
-		while(true) {
-		UI.printBoard(chessMath.getPieces());
-		System.out.println();
-		System.out.print("Source: ");
-		ChessPosition source = UI.readChessPosition(sc);
-		
-		System.out.println();
-		System.out.println("Target: ");
-		ChessPosition target = UI.readChessPosition(sc);
-		
-		ChessPiece capturedPiece = chessMath.performChessMove(source, target);
-		
+
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMath.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+	
+				System.out.println();
+				System.out.println("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+	
+				ChessPiece capturedPiece = chessMath.performChessMove(source, target);
+			}
+			catch(ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
-		
+
 	}
 
 }
